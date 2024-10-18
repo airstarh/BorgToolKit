32418c0650fc5c11d08567562d2ebbad78fbee5b
vsevolod azovsky <vsevolod.azovsky@softgamings.com>
2024-10-18 03:41:23 +0300

    Toral CSS

--- test/co.php
+++ test/co.php
@@ -1,17 +1,23 @@
 <?php
 require_once __DIR__ . '/../index.php';
 
-$a = "ASD [A:1]";
+$a = [
+    'key' =>
+        [
+            'a' => 'a',
+            'b' => 'b',
+        ],
+];
 
-$res = var_export($a, true);
-$res = mysqli_real_escape_string(null, $a);
+foreach ($a as $key => $v) {
+    $v['d'] = 'd';
+}
 
 echo PHP_EOL;
 echo PHP_EOL;
 echo '##################################################';
 echo PHP_EOL;
-###var_export($res);
-echo($res);
+var_export($a);
 echo PHP_EOL;
 echo '##################################################';
 echo PHP_EOL;
