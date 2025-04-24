8395e3a56f23c4260ee20c3161cad1f2255cd893
Vsevolod Azovsky <vsevolod.azovsky@softgamings.com>
2025-04-24 17:19:41 +0000

    dev

--- test/co.php
+++ test/co.php
@@ -1,16 +1,9 @@
 <?php
-require_once __DIR__ . '/../index.php';
 
-$float = 123.456789;
-$float = 109035655.60000000;
-$res['$float'] = $float;
-
-$number_format = number_format($float, 4, '.', '');
-$res['$number_format'] = $number_format;
-
-$sprintf = sprintf('%.4f', $float);
-$res['$sprintf'] = $sprintf;
+$a = json_decode('');
 
 echo PHP_EOL;
-var_export($res);
+echo PHP_EOL;
+var_export($a);
+echo PHP_EOL;
 echo PHP_EOL;
