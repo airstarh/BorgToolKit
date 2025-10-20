a928201049e681a0b349affa00a8629f7ddf6f44
Vsevolod Azovsky <vsevolod.azovsky@softmastery.net>
2025-10-20 04:13:10 +0000

    dev

--- test/co.php
+++ test/co.php
@@ -1,13 +1,25 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
-function a(){
-    echo 1;
-    register_shutdown_function('b');
-}
+####################################################################################################
 
-function b()
-{
-    echo 2;
-}
+$a = [
+    1,
+    1,
+    true,
+    'a',
+    null,
+];
 
-a();
+// $res = array_sum($a);
+$res = is_numeric(' 12');
+
+####################################################################################################
+echo PHP_EOL;
+echo PHP_EOL;
+var_export($res, 0);
+echo PHP_EOL;
+echo PHP_EOL;
+echo PHP_EOL;
