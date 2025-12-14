f29b951151ac52a6e9275785eee8b3941ca80f65
root <root@voronezhbook12.>
2025-12-14 23:10:11 +0300

    cert dev

--- programs/cert/inc/CertGenerator.php
+++ programs/cert/inc/CertGenerator.php
@@ -1,4 +1,6 @@
 <?php
+use function PHPUnit\Framework\returnArgument;
+?><?php
 
 class CertGenerator
 {
@@ -25,24 +27,21 @@ class CertGenerator
 
         foreach ($this->config as $field => $v) {
             if (false !== strpos($field, 'file')) {
-                $path = [
-                    $this->config->dist,
-                    $v
-                ];
-                $this->config->$field = implode(DIRECTORY_SEPARATOR, $path);
+                $this->config->$field = $this->distFile($v);
             }
         }
     }
 
     public function go()
     {
-        $counter = 0;
         foreach ($this->stackTemplate as $template) {
-            $counter++;
+            if ($template === '300-ext-configuration.php') {
+                $content = $this->buildCommand($template);
+                file_put_contents($this->config->fileExtfile, $content);
+                continue;
+            }
             $command = $this->buildCommand($template);
             $this->stackCommand[] = $command;
-            $distFile = $this->config->dist . DIRECTORY_SEPARATOR . "res-{$counter}";
-            file_put_contents($distFile, $command);
         }
         BorgDebug::dumpBeautiful($this->stackCommand);
 
@@ -54,5 +53,34 @@ class CertGenerator
         $file = $template;
         $path = realpath("{$folder}/{$file}");
         return BorgTemplate::render($path, $this->config);
+    }
+
+    public function distFolder()
+    {
+        $parts = [
+            $this->config->dist,
+            $this->config->CN,
+        ];
+        $path = implode(DIRECTORY_SEPARATOR, $parts);
+        $this->mkPathForce($path);
+        return $path;
+    }
+
+    public function distFile(string $fileName)
+    {
+        $parts = [
+            $this->distFolder(),
+            $fileName,
+        ];
+
+        return implode(DIRECTORY_SEPARATOR, $parts);
+    }
+    public function mkPathForce(string $path)
+    {
+        if (!is_dir($path)) {
+            if (!mkdir($path, 0755, true)) {
+                die('Failed to create directories...');
+            }
+        }
     }
 }
